de41a9e74570215e20e1a41a5483d4ae00ad5bbf
pcabreus <pcabreus@gmail.com>
2018-12-01 18:35:42 -0500

    restore services and doctrine removed

--- PositibeClassificationBundle.php
+++ PositibeClassificationBundle.php
@@ -14,26 +14,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PositibeClassificationBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $modelDir = realpath(__DIR__ . '/Resources/config/doctrine/model');
-
-        $mappings = array(
-            $modelDir => 'Positibe\Bundle\ClassificationBundle\Model',
-        );
-
-        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
-
-        if (class_exists($ormCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createXmlMappingDriver(
-                    $mappings,
-                    array('doctrine.orm.default_entity_manager')
-                )
-            );
-        }
-
-    }
 } 
